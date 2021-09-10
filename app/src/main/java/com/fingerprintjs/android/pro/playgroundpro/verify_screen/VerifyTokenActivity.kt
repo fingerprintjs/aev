9dd5e685dd02979db7574c7f06efd247bf02c86e
Alexey-Verkhovsky <alexey.verkhovsky@fingerprintjs.com>
2021-09-10 13:58:57 +0300

    Extend InstalledAppsSignal

--- app/src/main/java/com/fingerprintjs/android/pro/playgroundpro/verify_screen/VerifyTokenActivity.kt
+++
@@ -1,7 +0,0 @@
-package com.fingerprintjs.android.pro.playgroundpro.verify_screen
-
-import androidx.appcompat.app.AppCompatActivity
-
-class VerifyTokenActivity : AppCompatActivity() {
-
-}
--- application-verifier/src/main/java/com/fingerprintjs/android/pro/fingerprint/raw_signal_providers/PackageManagerInfoProvider.kt
+++ application-verifier/src/main/java/com/fingerprintjs/android/pro/fingerprint/raw_signal_providers/PackageManagerInfoProvider.kt
@@ -10,7 +10,8 @@ import java.security.cert.X509Certificate
 
 
 class CertificateInfo(
-    val dnameList: List<String>
+    val dnameList: List<String>,
+    val sigAlgInfo: List<String>
 )
 
 interface PackageManagerInfoProvider {
@@ -21,15 +22,17 @@ class PackageManagerInfoProviderImpl(
     private val packageManager: PackageManager
 ) : PackageManagerInfoProvider {
     override fun getCertificateInfo(packageName: String): CertificateInfo {
+        val certificates = getSigningCertificates(packageName)
         return CertificateInfo(
-            getSigningCertificates(packageName).map { it.issuerDN.name }
+            certificates.map { it.issuerDN.name },
+            certificates.map { it.sigAlgName }
         )
     }
 
     private fun getSigningCertificates(packageName: String): List<X509Certificate> {
         return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
             val packageInfo =
-                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
             packageInfo.signatures.mapNotNull {
                 extractX509FromSignature(it.toByteArray())
             }
