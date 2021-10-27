3a1ac63bd760f21207dd0df68faf07ec3011d932
Alexey-Verkhovsky <alexey.verkhovsky@fingerprintjs.com>
2021-10-27 14:56:30 +0300

    Fix ripple effect and dialog colors

--- app/src/main/java/com/fingerprintjs/android/pro/playgroundpro/dialogs/SettingsDialog.kt
+++ app/src/main/java/com/fingerprintjs/android/pro/playgroundpro/dialogs/SettingsDialog.kt
@@ -4,12 +4,13 @@ package com.fingerprintjs.android.pro.playgroundpro.dialogs
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.AlertDialog
-import android.app.Dialog
 import android.text.SpannableStringBuilder
 import android.view.LayoutInflater
 import android.widget.EditText
 import com.fingerprintjs.android.pro.playgroundpro.ApplicationPreferences
 import com.fingerprintjs.android.pro.playgroundpro.R
+import android.content.DialogInterface
+import androidx.core.content.ContextCompat
 
 
 class SettingsDialog(
@@ -17,7 +18,9 @@ class SettingsDialog(
     private val preferences: ApplicationPreferences
 ) {
 
-    private var dialog: Dialog? = null
+    private var dialog: AlertDialog? = null
+
+    private val accentColor = ContextCompat.getColor(activity, R.color.orange)
 
     @SuppressLint("InflateParams")
     fun showSettings() {
@@ -51,7 +54,14 @@ class SettingsDialog(
             .setView(view)
 
             .create()
+
         dialog?.show()
+
+        val negativeButton = dialog?.getButton(DialogInterface.BUTTON_NEGATIVE)
+        negativeButton?.setTextColor(accentColor)
+
+        val positiveButton = dialog?.getButton(DialogInterface.BUTTON_POSITIVE)
+        positiveButton?.setTextColor(accentColor)
     }
 
     fun dismiss() {
