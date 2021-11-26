d366b288462962a03bd1fee4f5e37469916cc205
Alexey-Verkhovsky <alexey.verkhovsky@fingerprintjs.com>
2021-11-26 11:51:35 +0300

    Add appName field

--- aev/src/main/java/com/fingerprintjs/android/aev/raw_signal_providers/PackageManagerInfoProvider.kt
+++ aev/src/main/java/com/fingerprintjs/android/aev/raw_signal_providers/PackageManagerInfoProvider.kt
@@ -23,34 +23,32 @@ interface PackageManagerInfoProvider {
 class PackageManagerInfoProviderImpl(
     private val packageManager: PackageManager
 ) : PackageManagerInfoProvider {
-    override fun getCertificateInfo(packageName: String): CertificateInfo {
+    override fun getCertificateInfo(packageName: String) = executeSafe({
         val certificates = getSigningCertificates(packageName)
-        return CertificateInfo(
+        CertificateInfo(
             certificates.map { it.issuerDN.name },
             certificates.map { it.sigAlgName }
         )
-    }
+    }, CertificateInfo(emptyList(), emptyList()))
 
     @SuppressLint("PackageManagerGetSignatures")
     private fun getSigningCertificates(packageName: String): List<X509Certificate> {
-        return executeSafe({
-            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
-                val packageInfo =
-                    packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
-                packageInfo.signatures.mapNotNull {
-                    extractX509FromSignature(it.toByteArray())
-                }
-            } else {
-                val packageInfo =
-                    packageManager.getPackageInfo(
-                        packageName,
-                        PackageManager.GET_SIGNING_CERTIFICATES
-                    )
-                packageInfo.signingInfo?.apkContentsSigners?.mapNotNull { signature ->
-                    extractX509FromSignature(signature.toByteArray())
-                }
-            } ?: emptyList()
-        }, emptyList())
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            val packageInfo =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+            packageInfo.signatures.mapNotNull {
+                extractX509FromSignature(it.toByteArray())
+            }
+        } else {
+            val packageInfo =
+                packageManager.getPackageInfo(
+                    packageName,
+                    PackageManager.GET_SIGNING_CERTIFICATES
+                )
+            packageInfo.signingInfo?.apkContentsSigners?.mapNotNull { signature ->
+                extractX509FromSignature(signature.toByteArray())
+            }
+        } ?: emptyList()
     }
 
     private fun extractX509FromSignature(byteArray: ByteArray): X509Certificate {
