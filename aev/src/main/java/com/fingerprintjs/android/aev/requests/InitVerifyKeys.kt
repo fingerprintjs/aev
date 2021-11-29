a81a8251173118e7b82787f194978e1e41e9817b
Alexey-Verkhovsky <alexey.verkhovsky@fingerprintjs.com>
2021-11-29 10:05:46 +0300

    Change API keys parameters name

--- aev/src/main/java/com/fingerprintjs/android/aev/requests/InitVerifyKeys.kt
+++ aev/src/main/java/com/fingerprintjs/android/aev/requests/InitVerifyKeys.kt
@@ -2,6 +2,6 @@ package com.fingerprintjs.android.aev.requests
 
 
 internal const val REQUEST_ID_KEY = "requestId"
-internal const val PUBLIC_API_KEY = "publicApiKey"
+internal const val PUBLIC_API_KEY = "publicKey"
 internal const val SIGNALS_KEY = "signals"
 internal const val APP_NAME_KEY = "appName"
--- app/src/main/java/com/fingerprintjs/android/aev/demo/demo_screen/api/Const.kt
+++ app/src/main/java/com/fingerprintjs/android/aev/demo/demo_screen/api/Const.kt
@@ -1,7 +1,7 @@
 package com.fingerprintjs.android.aev.demo.demo_screen.api
 
 
-internal const val PRIVATE_API_KEY = "privateApiKey"
+internal const val PRIVATE_API_KEY = "secretKey"
 internal const val REQUEST_ID_KEY = "requestId"
 internal const val RESULTS_KEY = "results"
 internal const val DEVICE_ID_KEY = "deviceId"
