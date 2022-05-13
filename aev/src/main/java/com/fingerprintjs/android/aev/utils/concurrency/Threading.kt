7a8a37e05b5587c01231444bd93f65c164f701a1
Sergey Makarov <sergeytop100@gmail.com>
2022-05-13 17:10:36 +0300

    Processing app packages in parallel manner

--- aev/src/main/java/com/fingerprintjs/android/aev/utils/concurrency/Threading.kt
+++ aev/src/main/java/com/fingerprintjs/android/aev/utils/concurrency/Threading.kt
@@ -2,6 +2,7 @@ package com.fingerprintjs.android.aev.utils.concurrency
 
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
+import kotlin.math.min
 
 private val executor = Executors.newCachedThreadPool()
 
@@ -44,4 +45,34 @@ internal fun <T> runInParallelVararg(
     return blocks
         .map { block -> executor.submit(Callable { block() }) }
         .map { future -> runCatching { future.get() } }
+}
+
+internal inline fun <T, R> Iterable<T>.mapParallel(
+    threadCount: Int = runCatching { Runtime.getRuntime().availableProcessors() }.getOrDefault(1),
+    crossinline transform: (T) -> R,
+): List<R> {
+    if (threadCount < 1) {
+        throw IllegalArgumentException("mapParallel: threadCount cannot be < 1, got: $threadCount")
+    }
+    val list = this.toList()
+    if (list.isEmpty()) {
+        return emptyList()
+    }
+    val subListSize = (list.size + threadCount - 1) / threadCount
+    val subListsCount = (list.size + subListSize - 1) / subListSize
+    val subLists =
+        (0 until subListsCount)
+            .map { subListIndex ->
+                list.subList(
+                    subListIndex * subListSize,
+                    min((subListIndex + 1) * subListSize, list.size)
+                )
+            }
+    val subListMappingLambdas = subLists.map { sublist ->
+        { sublist.map { transform(it) } }
+    }
+
+    val results = runInParallelVararg(*subListMappingLambdas.toTypedArray())
+
+    return results.map { it.getOrThrow() }.flatten()
 }
